dc0a1ad5f423882ef6f2086e93cc7a0f8a601be6
ShreyaMBelanekar42 <shreyaluvbelanekar@gmail.com>
2022-12-27 23:57:51 +0530

    displayed json data

---
+++ index.js
@@ -0,0 +1,18 @@
+fetch("featuredList.json")
+.then(function(response){
+    return response.json();
+})
+.then(function (items) {
+    let placeholder = document.querySelector(".grid-container");
+    let output = "";
+    for(let item of items){
+        output += `
+            <div class="grid-item">
+                <img src='${item.url}' />
+                <h3>${item.title}</h3>
+                <p>${item.price}</p>
+            </div>
+        `;
+    }
+    placeholder.innerHTML = output;
+})
